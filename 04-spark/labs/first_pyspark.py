eca362d00008a304599a81bb6344e0af5efff77a
Kevin <laopan123@foxmail.com>
2026-07-26 02:37:18 +0000

    learning window function

--- 04-spark/labs/first_pyspark.py
+++ 04-spark/labs/first_pyspark.py
@@ -1,43 +1,49 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql import functions as F
+from pyspark.sql.window import Window
 
 
 def main():
     spark = (
         SparkSession.builder
-        .appName("aggregation-basic")
+        .appName("window-function-basic")
         .getOrCreate()
     )
 
     data = [
-        ("Beijing", "A", 10),
-        ("Beijing", "A", 30),
-        ("Beijing", "B", 20),
-        ("Shanghai", "A", 40),
-        ("Shanghai", "B", 50),
-        ("Guangzhou", "A", 15)
+        ("Beijing", "order_001", 10),
+        ("Beijing", "order_002", 30),
+        ("Beijing", "order_003", 20),
+        ("Shanghai", "order_004", 40),
+        ("Shanghai", "order_005", 25),
+        ("Guangzhou", "order_006", 15)
     ]
 
-    columns = ["city", "category", "amount"]
+    columns = ["city", "order_id", "amount"]
 
     df: DataFrame = spark.createDataFrame(data, columns)
 
     print("1. Original Data")
     df.show()
 
-    print("2. Aggregation by city")
-    city_result:DataFrame = df.groupBy(F.col("city")).agg(F.sum(F.col("amount")).alias("total_amount")) 
-
-    city_result.show()
-
-    print("3. Aggregation by city and category")
-    city_category_result: DataFrame = df.groupBy("city", "category").agg(
-        F.sum("amount").alias("total_amount"),
-        F.count("*").alias("order_count"),
-        F.avg("amount").alias("avg_amount")
+    window_spec = Window.partitionBy("city").orderBy(
+        F.col("amount").desc()
     )
 
-    city_category_result.show()
+    print("2. Add row_number by city")
+    result: DataFrame = df.withColumn(
+        "rn",
+        F.row_number().over(window_spec)
+    )
+
+    result.show()
+
+    print("3. Top 1 order in each city")
+    top1_result: DataFrame = result.filter(
+        F.col("rn") == 1
+    )
+
+    top1_result.show()
 
     spark.stop()
 
